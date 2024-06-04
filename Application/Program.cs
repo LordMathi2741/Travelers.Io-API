8c8655cc95e0befd4cb1b0f6d94099d383dd3977
Mathias Jave Diaz <u202212721@upc.edu.pe>
2024-06-03 20:40:56 -0500

    docs: added api documentation

--- Application/Program.cs
+++ Application/Program.cs
@@ -1,3 +1,4 @@
+using System.Reflection;
 using Application.Mapper;
 using Domain.Interfaces;
 using Domain.Repository;
@@ -5,6 +6,7 @@ using Infrastructure.Context;
 using Infrastructure.Interfaces;
 using Infrastructure.MySql;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,7 +15,28 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+    {
+        options.SwaggerDoc("v1", new OpenApiInfo
+        {
+            Version = "v1",
+            Title = "Travelers.Io API",
+            Description = "Travelers.Io API documentation",
+            TermsOfService = new Uri("https://example.com/terms"),
+            Contact = new OpenApiContact
+            {
+                Name = "Example Contact",
+                Url = new Uri("https://example.com/contact")
+            },
+            License = new OpenApiLicense
+            {
+                Name = "Example License",
+                Url = new Uri("https://example.com/license")
+            }
+        });
+        var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+        options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+    });
 
 builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
 builder.Services.AddScoped(typeof(IRepositoryGeneric<>), typeof(RepositoryGeneric<>));
--- Application/Subscription/Controllers/PlansController.cs
+++ Application/Subscription/Controllers/PlansController.cs
@@ -9,16 +9,25 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Application.Subscription.Controllers;
 
+
 [Route("/api/v1/[controller]")]
 [ApiController]
 [Produces(MediaTypeNames.Application.Json)]
+[ProducesResponseType(404)]
+[ProducesResponseType(500)]
 [AllowAnonymous]
 public class PlansController(IRepositoryGeneric<Plan> repositoryGeneric, IMapper mapper) : ControllerBase
 {
     private readonly IRepositoryGeneric<Plan> _repositoryGeneric = repositoryGeneric;
     private readonly IMapper _mapper = mapper;
-
+    
+    // Post: api/v1/Plans
+    /// <summary>
+    /// Add a new plan
+    /// </summary>
+    
     [HttpPost]
+    [ProducesResponseType(201)]
     public async Task<IActionResult>AddPlan([FromBody] PlanRequest request)
     {
         var plan = _mapper.Map<PlanRequest, Plan>(request);
